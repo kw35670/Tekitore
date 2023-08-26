4f89507a1ccef97e27e670ca74ffc61bb9874cfc
riku <tkyu58690@gmail.com>
2023-08-26 14:28:23 +0900

    updated exhibit.html

--- exhibit/js/fileUpload.js
+++ exhibit/js/fileUpload.js
@@ -16,12 +16,36 @@ function previewFile(file) {
     // いざファイルをURLとして読み込む
     reader.readAsDataURL(file);
 }
-// <input>でファイルが選択されたときの処理
+// ファイルが選択されたときの処理
 const fileInput = document.getElementById("exhibitImg");
 const handleFileSelect = () => {
     const files = fileInput.files;
-    for (let i = 0; i < files.length; i++) {
-        previewFile(files[i]);
+    if (files.length <= 5) {
+        for (let i = 0; i < files.length; i++) {
+            previewFile(files[i]);
+            console.log(fileInput.files);
+        }
+    } else {
+        alert('画像枚数は最大５枚までです');
+        file.value = '';
+        console.log(fileInput.files);
     }
 };
-fileInput.addEventListener("change", handleFileSelect);
+fileInput.addEventListener("change", function () {
+    previewDelete();
+    handleFileSelect();
+});
+
+// ファイル削除機能
+const file = document.getElementById('exhibitImg');
+let preview = document.getElementById('preview');
+document.getElementById('clear').addEventListener('click', function() {
+    file.value = '';
+    previewDelete();
+})
+
+function previewDelete() {
+    while (preview.firstChild) {
+        preview.removeChild(preview.firstChild);
+    }
+}
